8f16e4f08b196769e7d7f93d94e39cee32d635a5
Alejandro Barrera López <93299015+vakdev@users.noreply.github.com>
2025-11-05 00:18:36 -0600

    Delete proveedor_model.py

--- proveedor_model.py
+++
@@ -1,9 +0,0 @@
-class ProveedroModel:
-    def __init__(self) -> None:
-        pass
-    def agregar_proveedor(self, nombre, telefono, direccion, correo, rfc, observaciones): pass
-    def obtener_proveedores(self): pass
-    def obtener_proveedor_por_id(self, id): pass
-    def actualizar_proveedor(self, id, nombre, telefono, direccion, correo, rfc, observaciones): pass
-    
-
